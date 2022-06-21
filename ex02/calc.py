3f2d0f600a7e5aeeaabd9cf7e16642ec2ee3d42a
KIN <c0a210425f@edu.teu.ac.jp>
2022-06-21 17:12:12 +0900

    問７

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,30 +1,17 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-import random 
 def button_click(event):
     btn=event.widget
     txt=btn["text"]
-    if txt=="=":
+    if txt!="=":
+        entry.insert(tk.END,txt)
+    else:
         q=entry.get()
-        q1=q.replace("×","*")
-        q2=q1.replace("÷","/")
-        a=eval(q2)
+        a=eval(q)
         entry.delete(0,tk.END)
         entry.insert(tk.END,a)
-    else:
-        entry.insert(tk.END,txt)
-def button_click2(event):
-    btn=event.widget
-    txt=btn["text"]
-    if "昼" in txt:
-        hiru=["マック","カレー","なし","いも虫","ステーキ","パスタ","チキン","パイモン",]
-        num=random.randint(0,7)
-        entry.delete(0,tk.END)
-        entry.insert(tk.END,hiru[num])
-
 root=tk.Tk()
-root.title("電卓")
-root.geometry("220x550")
+root.geometry("300x570")
 x=0
 y=1
 for i in range(9,-1,-1):
@@ -32,7 +19,7 @@ for i in range(9,-1,-1):
                     text=i,
                     width=4,
                     height=2,
-                    font=("Times New Roman", 20)
+                    font=("Times New Roman", 30)
     )
     btn.grid(row=y,column=x,)
     btn.bind("<1>",button_click)
@@ -41,25 +28,13 @@ for i in range(9,-1,-1):
     if x==3:
         x=0
         y+=1
-entry=tk.Entry(justify="right",width=10,font=("Times New Roman",30))
+entry=tk.Entry(justify="right",width=10,font=("Times New Roman",40))
 entry.grid(row=0,column=0,columnspan=3)
-btn2=tk.Button(root,text="+",width=4,height=2,font=("Times New Roman", 20))
+btn2=tk.Button(root,text="+",width=4,height=2,font=("Times New Roman", 30))
 btn2.grid(row=4,column=1)
 btn2.bind("<1>",button_click)
-btn3=tk.Button(root,text="=",width=4,height=2,font=("Times New Roman", 20))
+btn3=tk.Button(root,text="=",width=4,height=2,font=("Times New Roman", 30))
 btn3.grid(row=4,column=2)
 btn3.bind("<1>",button_click)
-btn4=tk.Button(root,text="-",width=4,height=2,font=("Times New Roman", 20))
-btn4.grid(row=5,column=0)
-btn4.bind("<1>",button_click)
-btn5=tk.Button(root,text="×",width=4,height=2,font=("Times New Roman", 20))
-btn5.grid(row=5,column=1)
-btn5.bind("<1>",button_click)
-btn6=tk.Button(root,text="÷",width=4,height=2,font=("Times New Roman", 20))
-btn6.grid(row=5,column=2)
-btn6.bind("<1>",button_click)
-btn7=tk.Button(root,text="昼",width=4,height=2,font=("Times New Roman", 20))
-btn7.grid(row=6,column=0)
-btn7.bind("<1>",button_click2)
 
 root.mainloop()
