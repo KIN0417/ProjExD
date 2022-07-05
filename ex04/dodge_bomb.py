cd22d0e012697bca7f12c4a3bf8bd44f8285703e
KIN <c0a210425f@edu.teu.ac.jp>
2022-07-05 15:47:10 +0900

    3限基本機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock=pg.time.Clock()
@@ -10,15 +11,70 @@ def main():
     bgimg_rct=bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc,bgimg_rct)
 
+    kkimg_sfc=pg.image.load("fig/6.png")
+    kkimg_sfc=pg.transform.rotozoom(kkimg_sfc,0,2.0)
+    kkimg_rct=kkimg_sfc.get_rect()
+    kkimg_rct.center=900,400
+
+    bmimg_sfc=pg.Surface((20,20))
+    bmimg_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bmimg_sfc,(255,0,0),(10,10),10)
+    bmimg_rct=bmimg_sfc.get_rect()
+    bmimg_rct.centerx=random.randint(0,screen_rct.width)
+    bmimg_rct.centery=random.randint(0,screen_rct.height)
+    vx,vy=+1,+1
 
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)
+        screen_sfc.blit(kkimg_sfc,kkimg_rct)
+        screen_sfc.blit(bmimg_sfc,bmimg_rct)
 
         for event in pg.event.get():
             if event.type==pg.QUIT:
                 return
+
+        key_states=pg.key.get_pressed()
+        if key_states[pg.K_UP]==True:
+            kkimg_rct.centery-=1
+        if key_states[pg.K_DOWN]==True:
+            kkimg_rct.centery+=1
+        if key_states[pg.K_LEFT]==True:
+            kkimg_rct.centerx-=1
+        if key_states[pg.K_RIGHT]==True:
+            kkimg_rct.centerx+=1
+        if check_bound(kkimg_rct,screen_rct)!=(1,1):
+            if key_states[pg.K_UP]==True:
+                kkimg_rct.centery+=1
+            if key_states[pg.K_DOWN]==True:
+                kkimg_rct.centery-=1
+            if key_states[pg.K_LEFT]==True:
+                kkimg_rct.centerx+=1
+            if key_states[pg.K_RIGHT]==True:
+                kkimg_rct.centerx-=1
+        
+        bmimg_rct.move_ip(vx,vy)            
+
+        yoko,tate= check_bound(bmimg_rct,screen_rct)
+        vx*=yoko
+        vy*=tate
+
+        if kkimg_rct.colliderect(bmimg_rct):
+            return
+
         pg.display.update()
         clock.tick(1000)
+
+
+def check_bound(rct,scr_rct):
+    yoko,tate=+1,+1
+    if rct.left<scr_rct.left or rct.right>scr_rct.right:
+        yoko=-1
+    if rct.top<scr_rct.top or rct.bottom>scr_rct.bottom:
+        tate=-1
+    return yoko,tate
+
+
+
 
 
 
